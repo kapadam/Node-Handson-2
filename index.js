1fefba837deab6fd7e31fc081594af0962416b0a
Kapadam <dharanidharu211@gmail.com>
2023-02-23 18:12:21 +0530

    created

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const server=express();
+const port=7878;
+
+server.get("/",(req,res)=>{
+    res.send("I am the Home page.");
+    res.end();
+})
+
+server.get("/api",(req,res)=>{
+    res.send("Express.js is a free and open-source web application framework for Node.js. It is used for designing and building web applications quickly and easily. Web applications are web apps that you can run on web browser. Since Express.js only requires javascript, it becomes easier for programmers and developers to build web applications and API without any effort.");
+    res.end();
+})
+
+server.get("/main",(req,res)=>{
+    res.send("Express.js is a framework of Node.js which means that most of the code is already written for programmers to work with. You can build a single page, multi-page, or hybrid web applications using Express.js. Express.js is lightweight and helps to organize web applications on the server-side into a more organized MVC architecture.");
+    res.end();
+})
+
+server.post("/submit",(req,res)=>{
+    res.send("POST worked.");
+    res.end();
+})
+
+server.listen(port,()=>{
+    console.log("Application is running...!!!");
+})
